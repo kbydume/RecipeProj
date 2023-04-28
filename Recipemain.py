f6dee9cfc1f2919ab5cc917ccc095ca3d7c98edf
kbydume <128094890+kbydume@users.noreply.github.com>
2023-04-27 20:24:11 -0400

    Initial Commit - Recipe main file

---
+++ Recipemain.py
@@ -0,0 +1,28 @@
+import csv
+import random
+from recipe import Recipe
+from recipebook import RecipeBook
+from manage import Manager
+
+def main():
+
+    while True:
+        print("\nWelcome to the recipe program!")
+        print("What would you like to do?")
+        print("1. Add a new recipe")
+        print("2. Get a recipe recommendation")
+        print("3. Delete a recipe")
+        print("4. Edit a recipe")
+        print("5. Quit")
+
+
+def add_recipe(recipe_book):
+
+def get_recommendation(recipe_book):
+
+def save_to_csv(recipe_book):
+    
+def load_from_csv(recipe_book):
+
+if __name__ == "__main__":
+    main()
