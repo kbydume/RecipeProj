523d4ac8df209df9213727886407cc3739a61bbe
kbydume <128094890+kbydume@users.noreply.github.com>
2023-04-28 19:29:33 -0400

    Updated docstrings in Unit Tests

--- test_recipe_program.py
+++ test_recipe_program.py
@@ -4,14 +4,19 @@ from recipebook import RecipeBook
 from manage import Manager
 
 class TestRecipeProgram(unittest.TestCase):
-
+    """This class tests methods in the overall recipe program."""
+    
     def test_recipe_creation(self):
+        """This test tests if a Recipe object is successfully made."""
         recipe = Recipe("Test Recipe", ["ingredient1", "ingredient2"], "Test instructions")
         self.assertEqual(recipe.get_title(), "Test Recipe")
         self.assertEqual(recipe.get_ingredients(), ["ingredient1", "ingredient2"])
         self.assertEqual(recipe.get_instructions(), "Test instructions")
 
     def test_recipebook_add_get_delete(self):
+        """This test tests the RecipeBook class's methods:
+        add_recipe(), get_all_recipes(), and remove_recipe() to see if they are 
+        working correctly. """
         recipe_book = RecipeBook()
         recipe = Recipe("Test Recipe", ["ingredient1", "ingredient2"], "Test instructions")
         recipe_book.add_recipe(recipe)
@@ -21,6 +26,7 @@ class TestRecipeProgram(unittest.TestCase):
         self.assertEqual(len(recipe_book.get_all_recipes()), 0)
 
     def test_manager_delete_recipe(self):
+        """This test tests the Manager class's delete_recipe() method."""
         recipe_book = RecipeBook()
         recipe = Recipe("Test Recipe", ["ingredient1", "ingredient2"], "Test instructions")
         recipe_book.add_recipe(recipe)
