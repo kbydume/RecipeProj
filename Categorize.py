7ef769067d21aff867d20b24ff4e8799f2d993e8
fraZong <128095343+fraZong@users.noreply.github.com>
2023-05-12 23:06:52 -0400

    Update Categorize.py

--- Categorize.py
+++ Categorize.py
@@ -4,6 +4,14 @@ import random
 
 class Categorize:
     def __init__(self, file_name):
+
+        """
+        Initialize the Categorize object.
+
+        Args:
+            file_name (str): The name of the CSV file containing recipes.
+        """
+
         self.file_name = file_name
         self.recipes = []
         self.meat_categories = {
@@ -28,12 +36,22 @@ class Categorize:
         self.read_csv()
 
     def read_csv(self):
+
+        """
+        Read the CSV file and populate the recipes list with its contents.
+        """
+
         with open(self.file_name, newline='', encoding='utf-8') as csvfile:
             reader = csv.reader(csvfile, delimiter=',', quotechar='"')
             next(reader)  # Skip the header row
             self.recipes = [row for row in reader]
 
     def categorize_recipes(self):
+
+        """
+        Categorize the recipes based on meat type and cooking style.
+        """
+
         for recipe in self.recipes:
             name = recipe[0]
             ingredients = recipe[1]
@@ -64,12 +82,41 @@ class Categorize:
                 self.cooking_styles['other'].append(name)
 
     def get_meat_categories(self):
+
+        """
+        Get the categorized recipes based on meat type.
+
+        Returns:
+            dict: A dictionary containing the categorized recipes based on meat type.
+        """
+
         return self.meat_categories
 
     def get_cooking_styles(self):
+
+        """
+        Get the categorized recipes based on cooking style.
+
+        Returns:
+            dict: A dictionary containing the categorized recipes based on cooking style.
+        """
+
         return self.cooking_styles
 
     def get_random_dishes_by_category(self, meat, style, num_dishes=2):
+
+        """
+        Get random dishes from the specified meat category and cooking style.
+
+        Args:
+            meat (str): The meat category.
+            style (str): The cooking style.
+            num_dishes (int, optional): The number of random dishes to retrieve. Defaults to 2.
+
+        Returns:
+            list: A list of random dish names.
+        """
+
         dishes_in_meat_category = self.meat_categories.get(meat, [])
         dishes_in_style_category = self.cooking_styles.get(style, [])
         
@@ -90,10 +137,23 @@ class Categorize:
         random_dishes = random.sample(combined_dishes, num_dishes)
         return random_dishes
 
+# ...
+
 def get_recommendations(meat, style):
+    
+    """
+    Get random dish recommendations based on the specified meat category and cooking style.
+
+    Args:
+        meat (str): The meat category.
+        style (str): The cooking style.
+
+    Returns:
+        list: A list of random dish names.
+    """
+
     file_name = 'recipe.csv'
     categorizer = Categorize(file_name)
     categorizer.categorize_recipes()
-    # recommendations = get_recommendations(meat, style)
     random_dishes = categorizer.get_random_dishes_by_category(meat, style, num_dishes=2)
     return random_dishes
